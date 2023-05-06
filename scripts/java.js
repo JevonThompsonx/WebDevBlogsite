883f0b2c392f7e3479d8369651f45d90a881c81f
JevonThompsonx <104575457+JevonThompsonx@users.noreply.github.com>
2023-05-06 01:09:31 -0400

    fixed dark mode toggle
     - problem: forgot to transfer toggler controller that chances icon on click
    
    - solution: transfered js code from old site

--- scripts/java.js
+++ scripts/java.js
@@ -1,3 +1,14 @@
+const togglerIcon = document.querySelector('.navbar-toggler-icon')
+let iconNumber = 0
+togglerIcon.addEventListener('click', ()=> {
+    iconNumber++
+    if (iconNumber % 2 === 0) { 
+      togglerIcon.innerHTML =  '<i class="fa-sharp fa-solid fa-angle-down"></i>'
+    }
+    else {
+    togglerIcon.innerHTML = '<i class="fa-solid fa-angle-up"></i>'
+    }
+})
 const welcomeImg = document.querySelector('#welcome-img')
 
 
