45dc47490acab7871f44c73d8d53f4bd97f46b6e
JevonThompsonx <104575457+JevonThompsonx@users.noreply.github.com>
2023-05-07 21:36:47 -0400

    Added all base blog stylings
    ###Blog is configured w/ everything except the data
    
     - 'realBlog' stylesheet added w/ all stylings of future blog
    
     - 'testBlog' will be used as a simple stylesheet to work w/ data as I'm learning how to interact w/ API data
    
     - blog pages moved to their own folder for organization since blogNav, nav, head and blogHead feels like clutter in the same folder
    
     - test data added to index.js for blog

--- index.js
+++ index.js
@@ -3,50 +3,109 @@ import ejs from 'ejs';
 import path from 'path';
 import fileDirName from './scripts/file-dir-name.js';
 const app = express();
-const {__dirname} = fileDirName(import.meta);
+const {
+    __dirname
+} = fileDirName(
+    import.meta);
 const port = 8080;
-app.use(express.static(path.join(__dirname,'')));
+app.use(express.static(path.join(__dirname, '')));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'/views'))
+app.set('views', path.join(__dirname, '/views'))
 
 app.listen(port);
 
-app.get('/', (req,res)=> {
+app.get('/', (req, res) => {
     try {
-res.render('index')
-    }
-    catch {
+        res.render('index')
+    } catch {
         res.statusCode(400)
     }
 })
 
-let blogPosts = [
+let blogPosts = [{
+        Title: 'My life as a developer',
+        date: '05/06/2023',
+        category: 'good news',
+        post: 'loremipsumstuff',
+        // postSnippet: post.slice(0, 15),
+        smallImage: 'urlHere.com',
+        largeImage: 'urlHere.com',
+        clicks: 0
+    }, {
+        Title: 'My javascript journey',
+        date: '05/01/2023',
+        category: 'good news',
+        post: 'loremipsumstuff',
+        // postSnippet: post.slice(0, 15),
+        smallImage: 'urlHere.com',
+        largeImage: 'urlHere.com',
+        clicks: 5
+    },
+    {
+        Title: 'FrontEnd vs Backend',
+        date: '05/31/2023',
+        category: 'good news',
+
+        post: 'loremipsumstuff',
+        // postSnippet: post.slice(0, 15),
+
+        smallImage: 'urlHere.com',
+        largeImage: 'urlHere.com',
+        clicks: 7
+    },
     {
         Title: 'My life as a developer',
         date: '05/06/2023',
-        post: 'loremipsumstuff'
-    },     {
+        category: 'good news',
+
+        post: 'loremipsumstuff',
+        // postSnippet: post.slice(0, 15),
+
+        smallImage: 'urlHere.com',
+        largeImage: 'urlHere.com',
+        clicks: 8
+    }, {
         Title: 'My javascript journey',
         date: '05/01/2023',
-        post: 'loremipsumstuff'
-    }, 
-    {
-        Title: 'JS',
-        date: '05/20/2023',
-        post: 'loremipsumstuff'
+        category: 'good news',
+
+        post: 'loremipsumstuff',
+        // postSnippet: post.slice(0, 15),
+
+        smallImage: 'urlHere.com',
+        largeImage: 'urlHere.com',
+        clicks: 2
     },
-        {
+    {
         Title: 'FrontEnd vs Backend',
         date: '05/31/2023',
-        post: 'loremipsumstuff'
-    },{
-        Title: 'nodeJS',
-        date: '05/25/2023',
-        post: 'loremipsumstuff'
+        category: 'good news',
+
+        post: 'loremipsumstuff',
+        // postSnippet: post.slice(0, 15),
+
+        smallImage: 'urlHere.com',
+        largeImage: 'urlHere.com',
+        clicks: 3
     }
-    
+
 ]
 
-app.get('/WebDevBlog', (req,res)=> {
-    res.render('blog', {blogPosts})
+app.get('/WebDevBlog', (req, res) => {
+    let newPosts= blogPosts.slice(0,4)
+    res.render('blogPage/realBlog', {
+        blogPosts, newPosts
+    })
+})
+app.get('/WebDevBlog/:post', (req, res) => {
+    let {
+        post
+    } = req.params
+    //search for title in database and return the result
+    //as the page to load
+    //send page to 'post' when rendering as a variable
+    res.render('/blogPage/post', {
+        post,
+        blogPost,
+    })
 })
