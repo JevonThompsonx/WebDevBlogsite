8bf392aee3512ad63ce1fb4128fce5e3bdf9c306
JevonThompsonx <104575457+JevonThompsonx@users.noreply.github.com>
2023-05-14 19:56:19 -0400

    link to main website update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import ejs from 'ejs';
 import path from 'path';
 import fileDirName from './scripts/file-dir-name.js';
+import { v4 as uuid } from 'uuid';
 
 const app = express();
 const {
@@ -13,7 +14,9 @@ app.use(express.static(path.join(__dirname, '')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 app.listen(port);
-app.use(express.urlencoded({ extended: true })) 
+app.use(express.urlencoded({
+    extended: true
+}))
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -24,7 +27,9 @@ app.get('/', (req, res) => {
     }
 })
 
-let blogPosts = [{
+let blogPosts = [
+    {
+        id: uuid(),
         title: 'My life as a developer',
         date: '05/06/2023',
         category: 'good news',
@@ -34,6 +39,8 @@ let blogPosts = [{
         largeImage: 'urlHere.com',
         clicks: 0
     }, {
+        id: uuid(),
+
         title: 'My javascript journey',
         date: '05/01/2023',
         category: 'good news',
@@ -44,11 +51,14 @@ let blogPosts = [{
         clicks: 5
     },
     {
+        id: uuid(),
+
         title: 'FrontEnd vs Backend',
         date: '05/31/2023',
         category: 'good news',
 
         post: 'loremipsumstuff',
+        
         // postSnippet: post.slice(0, 15),
 
         smallImage: 'urlHere.com',
@@ -56,6 +66,8 @@ let blogPosts = [{
         clicks: 7
     },
     {
+        id: uuid(),
+
         title: 'My life as a developer',
         date: '05/06/2023',
         category: 'good news',
@@ -67,6 +79,8 @@ let blogPosts = [{
         largeImage: 'urlHere.com',
         clicks: 8
     }, {
+        id: uuid(),
+
         title: 'My javascript journey',
         date: '05/01/2023',
         category: 'good news',
@@ -79,6 +93,8 @@ let blogPosts = [{
         clicks: 2
     },
     {
+        id: uuid(),
+
         title: 'FrontEnd vs Backend',
         date: '05/31/2023',
         category: 'good news',
@@ -94,17 +110,47 @@ let blogPosts = [{
 ]
 
 app.get('/WebDevBlog', (req, res) => {
-    let newPosts= blogPosts.slice(0,5)
+    let newPosts = blogPosts.slice(0, 5)
     res.render('blogPage/realBlog', {
-        blogPosts, newPosts
+        blogPosts,
+        newPosts
     })
 })
-app.get('/WebDevBlog/newPost', (req,res)=> {
-        res.render('blogPage/posts')
+app.get('/WebDevBlog/newPost', (req, res) => {
+    res.render('blogPage/newPost')
 })
 app.post('/WebDevBlog', (req, res) => {
-    let {title , date, category, post, smallImage, largeImage} = req.body
-    blogPosts.push({title , date, category, post, smallImage, largeImage})
+    let {
+        title,
+        date,
+        category,
+        post,
+        smallImage,
+        largeImage
+    } = req.body;
+    let id = uuid()
+    blogPosts.push({
+        title,
+        date,
+        category,
+        post,
+        smallImage,
+        largeImage, 
+        id
+    })
     console.log(req.body)
     res.send(blogPosts)
+})
+
+app.get('/WebDevBlog/:postId', (req, res) => {
+    let {
+        postId: id
+    } = req.params
+    const singlePost = blogPosts.find(post => {
+     return  post.id === id
+    })
+    console.log(singlePost)
+    res.render('blogPage/showPost', {
+        ...singlePost
+    })
 })
